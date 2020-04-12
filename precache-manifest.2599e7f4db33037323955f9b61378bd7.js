08ac82aec8b9d35995f9a1485ef33155b01be692
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2020-04-12 20:53:54 +0400

    Updates

---
+++ precache-manifest.2599e7f4db33037323955f9b61378bd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031f0e6101a3d854f810c2e56ead6ed3",
+    "url": "/Covid19-tracker/index.html"
+  },
+  {
+    "revision": "43bbbabfae48fff587f9",
+    "url": "/Covid19-tracker/static/css/main.687adcd2.chunk.css"
+  },
+  {
+    "revision": "5b73c9433268f8cd8e49",
+    "url": "/Covid19-tracker/static/js/2.737e45f4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-tracker/static/js/2.737e45f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43bbbabfae48fff587f9",
+    "url": "/Covid19-tracker/static/js/main.545c00ce.chunk.js"
+  },
+  {
+    "revision": "f4c1ab6ece6593f15c62",
+    "url": "/Covid19-tracker/static/js/runtime-main.bafe08b4.js"
+  },
+  {
+    "revision": "785a5a032e08545658635cd609860142",
+    "url": "/Covid19-tracker/static/media/corona.785a5a03.jpg"
+  }
+]);
